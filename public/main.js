a43bd73c4ae350efc6eb80afde4ed71d39c2c360
dom8888912 <github@472a.de>
2025-09-07 20:42:53 +0200

    feat: support automatic light and dark theme

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 
-// ui-version:2025-08-23-v13 – Menü-Overlay, Preisindikator ohne Graph, Suche & Cookie-Banner unten
+// ui-version:2025-08-23-v15 – Menü-Overlay, Preisindikator ohne Graph, Suche & Cookie-Banner unten
 (function(){
   // Menü
   var btn = document.getElementById('nav-toggle');
